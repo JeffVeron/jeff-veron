1f9af6dfed2a034feab79c705d5314903702a629
JeffVeron <67024868+JeffVeron@users.noreply.github.com>
2023-01-19 22:43:46 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import portfolio2 from "./fakeImage2.jpeg";
 import portfolio3 from "./FakeImages.jpeg";
 import portfolio4 from "./Fakeimages4.jpeg";
 import { Routes, Route, /*  Outlet, */ Link } from "react-router-dom";
-import { Analytics } from '@vercel/analytics';
+import { analytics } from '@vercel/analytics';
 
  
 
